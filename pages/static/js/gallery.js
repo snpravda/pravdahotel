58b5f62bf251e4a2515a1a88f01a687a9db63788
snpravda <sn.pravda@gmail.com>
2020-12-04 10:04:47 +0200

    fixed js behaviour

--- pages/static/js/gallery.js
+++ pages/static/js/gallery.js
@@ -14,27 +14,27 @@ function hotel( ){
   h2 = document.getElementById('h2');
   h3 = document.getElementById('h3');
 
-  h1.setAttribute('src', "{% static 'img/1h.jpg' %}");
-  h2.setAttribute('src', "{% static 'img/2h.jpg' %}");
-  h3.setAttribute('src', "{% static 'img/3h.jpg' %}");
+  h1.setAttribute('src', "/static/img/1h.jpg");
+  h2.setAttribute('src', "/static/img/2h.jpg");
+  h3.setAttribute('src', "/static/img/3h.jpg");
 
 }
 function aquapark(){
 
- h1.setAttribute('src', "{% static 'img/aq1.jpg' %}")
- h2.setAttribute('src', "{% static 'img/aq2.jpg' %}")
- h3.setAttribute('src', "{% static 'img/aq3.jpg' %}")
+ h1.setAttribute('src', "/static/img/aq1.jpg")
+ h2.setAttribute('src', "/static/img/aq2.jpg")
+ h3.setAttribute('src', "/static/img/aq3.jpg")
 }
 function rest(){
 
 
-  h1.setAttribute('src', "{% static 'img/re1.jpg' %}")
-  h2.setAttribute('src', "{% static 'img/re2.jpg' %}")
-  h3.setAttribute('src', "{% static 'img/re3.jpg' %}")
+  h1.setAttribute('src', "/static/img/re1.jpg")
+  h2.setAttribute('src', "/static/img/re2.jpg")
+  h3.setAttribute('src', "/static/img/re3.jpg")
 }
 function spa(){
 
-  h1.setAttribute('src', "{% static 'img/spa1.jpg' %}")
-  h2.setAttribute('src', "{% static 'img/spa2.jpg' %}")
-  h3.setAttribute('src', "{% static 'img/spa3.jpg' %}")
+  h1.setAttribute('src', "/static/img/spa1.jpg")
+  h2.setAttribute('src', "/static/img/spa2.jpg")
+  h3.setAttribute('src', "/static/img/spa3.jpg")
 }
