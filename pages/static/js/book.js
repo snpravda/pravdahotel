d197315c291b4eb9f9152683f8a330867c7a9e68
snpravda <sn.pravda@gmail.com>
2020-12-10 10:16:32 +0200

    commented not working form validation

--- pages/static/js/book.js
+++ pages/static/js/book.js
@@ -69,19 +69,19 @@ function rooms() {
 //     return true;
 // }
 
-function setMinToday() {
-    var today = new Date();
-    var dd = today.getDate();
-    var mm = today.getMonth() + 1; //January is 0!
-    var yyyy = today.getFullYear();
-    if (dd < 10) {
-        dd = '0' + dd
-    }
-    if (mm < 10) {
-        mm = '0' + mm
-    }
-
-    today = yyyy + '-' + mm + '-' + dd;
-    document.getElementsByName("arrive").min = today;
-    document.getElementsByName("departure").min = today;
-}
+// function setMinToday() {
+//     var today = new Date();
+//     var dd = today.getDate();
+//     var mm = today.getMonth() + 1; //January is 0!
+//     var yyyy = today.getFullYear();
+//     if (dd < 10) {
+//         dd = '0' + dd
+//     }
+//     if (mm < 10) {
+//         mm = '0' + mm
+//     }
+//
+//     today = yyyy + '-' + mm + '-' + dd;
+//     document.getElementsByName("arrive").min = today;
+//     document.getElementsByName("departure").min = today;
+// }
