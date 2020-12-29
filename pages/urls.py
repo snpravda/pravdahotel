8d0645986fe7fcaf0036b7d983d7ab3fb96d7947
snpravda <sn.pravda@gmail.com>
2020-12-30 00:26:19 +0200

    replaces _ with - in urls

--- pages/urls.py
+++ pages/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about_us/', views.about_us, name='about_us'),
+    path('about-us/', views.about_us, name='about-us'),
     path('gallery/', views.gallery, name='gallery'),
     path('book/', views.book, name='book'),
 ]
